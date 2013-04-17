707c5ca18522718438e116d7b9267e018f08389d
Alexander Gladysh <agladysh@gmail.com>
2013-04-18 00:25:22 +0400

    rit2013_ag: initial commit

---
+++ rit2013_ag/make.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# sudo port install texlive-xetex
+# sudo port install texlive-lang-cyrillic
+# sudo port install texlive-latex-extra
+# sudo port install texlive-fonts-recommended
+xelatex rit2013_ag.tex
