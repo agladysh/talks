637077d5e53abff5b30ce6ab10bf4d6c9dfc6a85
Alexander Gladysh <agladysh@gmail.com>
2015-11-05 17:32:16 +0300

    luaws15_ag: installation instructions

--- luaws15_ag/make.sh
+++ luaws15_ag/make.sh
@@ -1,4 +1,7 @@
 # /bin/bash
 
-# See minted docs for installation instructions
+# Ubuntu:
+# sudo apt-get install texlive-full python-pygments
+
+# Other OSes: See minted docs for installation instructions
 pdflatex -shell-escape luaws15_ag.tex
