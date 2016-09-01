28857152b3d5067a2f3a4d06a70bea79380e4b60
Alexander Gladysh <agladysh@gmail.com>
2016-09-01 08:48:27 +0300

    luaim_sep16_ag: draft slides for the Lua in Moscow talk

---
+++ luaim_sep16_ag/make.sh
@@ -0,0 +1,7 @@
+# /bin/bash
+
+# Ubuntu:
+# sudo apt-get install texlive-full python-pygments
+
+# Other OSes: See minted docs for installation instructions
+pdflatex -shell-escape luaim_sep16_ag.tex
