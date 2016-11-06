50b9387379724818250eae1996eee4c2a8bdb97e
Alexander Gladysh <agladysh@gmail.com>
2016-11-06 16:51:11 +0300

    hl16: initial commit

---
+++ highload2016_ag/make.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# sudo port install texlive-xetex
+# sudo port install texlive-lang-cyrillic
+# sudo port install texlive-latex-extra
+# sudo port install texlive-fonts-recommended
+xelatex highload2016_ag.tex
