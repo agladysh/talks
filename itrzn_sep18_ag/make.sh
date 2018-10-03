3b7cb383c551f36b71645dbf5a8eca029efc4640
Alexander Gladysh <agladysh@gmail.com>
2018-10-03 10:53:58 +0300

    itrzn sep18 talk added

---
+++ itrzn_sep18_ag/make.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+
+# Ubuntu:
+# sudo apt-get install texlive-full python-pygments
+
+# OS X
+# brew install mactex
+# sudo easy_install Pygments
+
+# Other OSes: See minted docs for installation instructions
+
+pdflatex -shell-escape itrzn_sep18_ag.tex
